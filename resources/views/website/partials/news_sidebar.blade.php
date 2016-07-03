07f6ab5cf6e34ea89a8632aacba6e6ca092692a7
marco asperti <marcoasperti@gmail.com>
2016-07-03 14:30:09 +0200

    #UPDATE

--- resources/views/website/partials/news_sidebar.blade.php
+++ resources/views/website/partials/news_sidebar.blade.php
@@ -1,9 +1,9 @@
-<!--=== news side bar ===-->
+    <!--=== news side bar ===-->
 @inject('posts','App\News')
 <div class="col-md-3 mb25-max-sm">
-    <h3 class="color-4  mb15">{{ $article->title }}</h3>
+    <h3 class="color-4  mb15">Latest news</h3>
     <div class="text-left wow flipInY animated  animated">
-        <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( $article->slug )) }}"><h2 class="color-main mb25 txt-upper text-extra-spaced-mid">{{ $article->subtitle }}</h2></a>
+        <a href="{{ LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( 'news' )) }}"><h2 class="color-main mb25 txt-upper text-extra-spaced-mid">{{ $article->subtitle }}</h2></a>
     </div>
     @foreach (  $posts->Latest(3)->get() as  $index => $posta )
         <div class="media mb15 pb15 border-bottom-color-5">
