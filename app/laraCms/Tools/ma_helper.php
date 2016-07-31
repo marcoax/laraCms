80b83b2bda1a55c2713c4480bf653e05f3ead043
marco asperti <marcoasperti@gmail.com>
2016-07-31 10:00:05 +0200

    fix
    fix crash when image are not present

--- app/laraCms/Tools/ma_helper.php
+++ app/laraCms/Tools/ma_helper.php
@@ -1,100 +1,114 @@
 <?php
-	/*********************  doc **********************/
-	function ma_get_doc_from_repository($doc){
-		$path  = config('laraCms.admin.path.doc_repository');
-		return asset($path.$doc);
-	}
+/*********************  doc **********************/
+function ma_get_doc_from_repository($doc)
+{
+    $path = config('laraCms.admin.path.doc_repository');
+    return asset($path . $doc);
+}
 
-    /********      image    *****************/
-	function ma_get_image_from_repository($img){
-		
-		$path  =config('laraCms.admin.path.img_repository');
-		return asset($path.$img); 
-	}
+/********      image    *****************/
+function ma_get_image_from_repository($img)
+{
 
-	/********      image    *****************/
-	function ma_get_image_on_the_fly($asset,$w,$h,$type='jpg'){
+    $path = config('laraCms.admin.path.img_repository');
+    return asset($path . $img);
+}
 
-		if($asset!='') {
-			$img = Image::make(ma_get_image_from_repository($asset) )->fit($w,$h)->encode($type);
-			return  'data:image/' . $type . ';base64,' . base64_encode($img);
-		}
-		else return null;
-	}
-	/********      image    *****************/
-	function ma_get_image_on_the_fly_chached($asset,$w,$h,$type='jpg'){
-		if($asset!='') {
-			$dataImage = array();
-			$dataImage['asset'] = $asset;
-			$dataImage['w'] = $w;
-			$dataImage['h'] = $h;
-			$dataImage['type'] = $type;
-			$img = Image::cache(function($image) use ($dataImage) {
-				$image->make(ma_get_image_from_repository($dataImage['asset']))->fit($dataImage['w'],$dataImage['h'])->encode($dataImage['type']);
-			});
-			return  'data:image/' . $type . ';base64,' . base64_encode($img);
-		}
-		else return null;
-	}
+/********      image    *****************/
+function ma_get_image_on_the_fly($asset, $w, $h, $type = 'jpg')
+{
 
-	
-	/*******************  admin   ***************/
-	function ma_get_admin_list_url( $model ){
-		$path      = '/admin/list';
-		$modelName = (!is_object( $model )) ? strtolower( $model ) : strtolower(str_plural( class_basename($model) ));
-		return URL::to($path.'/'.str_plural($modelName)); 
-	}
+    if ($asset != '') {
+        $img = Image::make(ma_get_image_from_repository($asset))->fit($w, $h)->encode($type);
+        return 'data:image/' . $type . ';base64,' . base64_encode($img);
+    } else return null;
+}
 
-	function ma_get_admin_create_url( $model ){
-		$path      = '/admin/create';
-		$modelName = (!is_object( $model )) ? strtolower( $model ) : strtolower(str_plural( class_basename($model) ));
-		return URL::to($path.'/'.str_plural($modelName));
-	}
-	function ma_get_admin_edit_url( $model ){
-		$path      = '/admin/edit';
-		$modelName = (!is_object( $model )) ? strtolower( $model ) : strtolower(str_plural( class_basename($model) ));
-		return URL::to($path.'/'.str_plural($modelName).'/'.$model->id);
-	}
-	function ma_get_admin_editmodal_url( $model ){
-		$path      = '/admin/editmodal';
-		$modelName = (!is_object( $model )) ? strtolower( $model ) : strtolower(str_plural( class_basename($model) ));
-		return URL::to($path.'/'.str_plural($modelName).'/'.$model->id);
-	}
-	function ma_get_admin_delete_url( $model ){
-		$path      = '/admin/delete';
-		$modelName = (!is_object( $model )) ? strtolower( $model ) : strtolower(str_plural( class_basename($model) ));
-		return URL::to($path.'/'.str_plural($modelName).'/'.$model->id);
-	}
+/********      image    *****************/
+function ma_get_image_on_the_fly_chached($asset, $w, $h, $type = 'jpg')
+{
+    if ($asset != '') {
+        $dataImage = array();
+        $dataImage['asset'] = $asset;
+        $dataImage['w'] = $w;
+        $dataImage['h'] = $h;
+        $dataImage['type'] = $type;
+        if (file_exists($dataImage['asset'])) {
+            $img = Image::cache(function ($image) use ($dataImage) {
+                $image->make(ma_get_image_from_repository($dataImage['asset']))->fit($dataImage['w'], $dataImage['h'])->encode($dataImage['type']);
+            });
+            return 'data:image/' . $type . ';base64,' . base64_encode($img);
+        } else return null;
+    } else return null;
+}
 
-	function ma_get_admin_preview_url( $model ){
 
-		$modelName    = (!is_object( $model )) ?   strtolower( $model ) : strtolower(str_plural( class_basename($model) ));
-		$resourcePath = ( $modelName!='articles')? str_plural($modelName).'/'.$model->slug : $model->slug;
-		$path = LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(),URL::to( $resourcePath ));
-		return URL::to($path);
-	}
+/*******************  admin   ***************/
+function ma_get_admin_list_url($model)
+{
+    $path = '/admin/list';
+    $modelName = (!is_object($model)) ? strtolower($model) : strtolower(str_plural(class_basename($model)));
+    return URL::to($path . '/' . str_plural($modelName));
+}
 
-	/**
-	 * Is the mime type an image
-	 */
-	function is_image($mimeType)
-	{
-		return starts_with($mimeType, 'image/');
-	}
+function ma_get_admin_create_url($model)
+{
+    $path = '/admin/create';
+    $modelName = (!is_object($model)) ? strtolower($model) : strtolower(str_plural(class_basename($model)));
+    return URL::to($path . '/' . str_plural($modelName));
+}
 
-if ( ! function_exists('flash')) {
-	/**
-	 * Arrange for a flash message.
-	 *
-	 * @param  string|null $message
-	 * @return \Laracasts\Flash\FlashNotifier
-	 */
-	function flash($message = null)
-	{
-		$notifier = app('flash');
-		if ( ! is_null($message)) {
-			return $notifier->info($message);
-		}
-		return $notifier;
-	}
+function ma_get_admin_edit_url($model)
+{
+    $path = '/admin/edit';
+    $modelName = (!is_object($model)) ? strtolower($model) : strtolower(str_plural(class_basename($model)));
+    return URL::to($path . '/' . str_plural($modelName) . '/' . $model->id);
+}
+
+function ma_get_admin_editmodal_url($model)
+{
+    $path = '/admin/editmodal';
+    $modelName = (!is_object($model)) ? strtolower($model) : strtolower(str_plural(class_basename($model)));
+    return URL::to($path . '/' . str_plural($modelName) . '/' . $model->id);
+}
+
+function ma_get_admin_delete_url($model)
+{
+    $path = '/admin/delete';
+    $modelName = (!is_object($model)) ? strtolower($model) : strtolower(str_plural(class_basename($model)));
+    return URL::to($path . '/' . str_plural($modelName) . '/' . $model->id);
+}
+
+function ma_get_admin_preview_url($model)
+{
+
+    $modelName = (!is_object($model)) ? strtolower($model) : strtolower(str_plural(class_basename($model)));
+    $resourcePath = ($modelName != 'articles') ? str_plural($modelName) . '/' . $model->slug : $model->slug;
+    $path = LaravelLocalization::getLocalizedURL(LaravelLocalization::getCurrentLocale(), URL::to($resourcePath));
+    return URL::to($path);
+}
+
+/**
+ * Is the mime type an image
+ */
+function is_image($mimeType)
+{
+    return starts_with($mimeType, 'image/');
+}
+
+if (!function_exists('flash')) {
+    /**
+     * Arrange for a flash message.
+     *
+     * @param  string|null $message
+     * @return \Laracasts\Flash\FlashNotifier
+     */
+    function flash($message = null)
+    {
+        $notifier = app('flash');
+        if (!is_null($message)) {
+            return $notifier->info($message);
+        }
+        return $notifier;
+    }
 }
