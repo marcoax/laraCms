37004b6fb175d0cb63cf433ae4852d5255e38246
marco asperti <marcoasperti@gmail.com>
2016-08-13 16:23:42 +0200

    add clockwork support

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     */
 
     'debug' => env('APP_DEBUG', TRUE),
-	'env' => env('APP_ENV', 'production'),
+    'env' => env('APP_ENV', 'production'),
 
     /*
     |--------------------------------------------------------------------------
@@ -145,19 +145,19 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-		/*
-		*  Vendor
-		*/
-		'Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider',
-		Dimsav\Translatable\TranslatableServiceProvider::class,
-		'hisorange\BrowserDetect\Provider\BrowserDetectService',
-		Collective\Html\HtmlServiceProvider::class,
-		'Zizaco\Entrust\EntrustServiceProvider',
-		'Intervention\Image\ImageServiceProvider',
-		 Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class,
-         Collective\Bus\BusServiceProvider::class,
-         Maatwebsite\Excel\ExcelServiceProvider::class,
-
+        /*
+        *  Vendor
+        */
+        'Mcamara\LaravelLocalization\LaravelLocalizationServiceProvider',
+        Dimsav\Translatable\TranslatableServiceProvider::class,
+        'hisorange\BrowserDetect\Provider\BrowserDetectService',
+        Collective\Html\HtmlServiceProvider::class,
+        'Zizaco\Entrust\EntrustServiceProvider',
+        'Intervention\Image\ImageServiceProvider',
+        Artesaos\SEOTools\Providers\SEOToolsServiceProvider::class,
+        Collective\Bus\BusServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        'Clockwork\Support\Laravel\ClockworkServiceProvider',
 
         // laraCMS
         App\laraCms\Admin\Providers\AdminFormServiceProvider::class,
@@ -169,7 +169,7 @@ return [
         App\laraCms\Website\Providers\AppServiceProvider::class,
         App\laraCms\Website\Providers\DecoratorServiceProvider::class,
 
-	],
+    ],
 
     /*
     |--------------------------------------------------------------------------
@@ -184,64 +184,64 @@ return [
 
     'aliases' => [
 
-        'App'       => Illuminate\Support\Facades\App::class,
-        'Artisan'   => Illuminate\Support\Facades\Artisan::class,
-        'Auth'      => Illuminate\Support\Facades\Auth::class,
-        'Blade'     => Illuminate\Support\Facades\Blade::class,
-        'Bus'       => Illuminate\Support\Facades\Bus::class,
-        'Cache'     => Illuminate\Support\Facades\Cache::class,
-        'Config'    => Illuminate\Support\Facades\Config::class,
-        'Cookie'    => Illuminate\Support\Facades\Cookie::class,
-        'Crypt'     => Illuminate\Support\Facades\Crypt::class,
-        'DB'        => Illuminate\Support\Facades\DB::class,
-        'Eloquent'  => Illuminate\Database\Eloquent\Model::class,
-        'Event'     => Illuminate\Support\Facades\Event::class,
-        'File'      => Illuminate\Support\Facades\File::class,
-        'Hash'      => Illuminate\Support\Facades\Hash::class,
-        'Input'     => Illuminate\Support\Facades\Input::class,
+        'App' => Illuminate\Support\Facades\App::class,
+        'Artisan' => Illuminate\Support\Facades\Artisan::class,
+        'Auth' => Illuminate\Support\Facades\Auth::class,
+        'Blade' => Illuminate\Support\Facades\Blade::class,
+        'Bus' => Illuminate\Support\Facades\Bus::class,
+        'Cache' => Illuminate\Support\Facades\Cache::class,
+        'Config' => Illuminate\Support\Facades\Config::class,
+        'Cookie' => Illuminate\Support\Facades\Cookie::class,
+        'Crypt' => Illuminate\Support\Facades\Crypt::class,
+        'DB' => Illuminate\Support\Facades\DB::class,
+        'Eloquent' => Illuminate\Database\Eloquent\Model::class,
+        'Event' => Illuminate\Support\Facades\Event::class,
+        'File' => Illuminate\Support\Facades\File::class,
+        'Hash' => Illuminate\Support\Facades\Hash::class,
+        'Input' => Illuminate\Support\Facades\Input::class,
         'Inspiring' => Illuminate\Foundation\Inspiring::class,
-        'Lang'      => Illuminate\Support\Facades\Lang::class,
-        'Log'       => Illuminate\Support\Facades\Log::class,
-        'Mail'      => Illuminate\Support\Facades\Mail::class,
-        'Password'  => Illuminate\Support\Facades\Password::class,
-        'Queue'     => Illuminate\Support\Facades\Queue::class,
-        'Redirect'  => Illuminate\Support\Facades\Redirect::class,
-        'Redis'     => Illuminate\Support\Facades\Redis::class,
-        'Request'   => Illuminate\Support\Facades\Request::class,
-        'Response'  => Illuminate\Support\Facades\Response::class,
-        'Route'     => Illuminate\Support\Facades\Route::class,
-        'Schema'    => Illuminate\Support\Facades\Schema::class,
-        'Session'   => Illuminate\Support\Facades\Session::class,
-        'Storage'   => Illuminate\Support\Facades\Storage::class,
-        'URL'       => Illuminate\Support\Facades\URL::class,
+        'Lang' => Illuminate\Support\Facades\Lang::class,
+        'Log' => Illuminate\Support\Facades\Log::class,
+        'Mail' => Illuminate\Support\Facades\Mail::class,
+        'Password' => Illuminate\Support\Facades\Password::class,
+        'Queue' => Illuminate\Support\Facades\Queue::class,
+        'Redirect' => Illuminate\Support\Facades\Redirect::class,
+        'Redis' => Illuminate\Support\Facades\Redis::class,
+        'Request' => Illuminate\Support\Facades\Request::class,
+        'Response' => Illuminate\Support\Facades\Response::class,
+        'Route' => Illuminate\Support\Facades\Route::class,
+        'Schema' => Illuminate\Support\Facades\Schema::class,
+        'Session' => Illuminate\Support\Facades\Session::class,
+        'Storage' => Illuminate\Support\Facades\Storage::class,
+        'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
-        'View'      => Illuminate\Support\Facades\View::class,
+        'View' => Illuminate\Support\Facades\View::class,
 
-		/*
+        /*
         *  Vendor
         */
-		'LaravelLocalization'   => 'Mcamara\LaravelLocalization\Facades\LaravelLocalization',
-    	'BrowserDetect' => 'hisorange\BrowserDetect\Facade\Parser',
-    	'Form'    => Collective\Html\FormFacade::class,
-    	'Html'    => Collective\Html\HtmlFacade::class,
-    	'Entrust' => 'Zizaco\Entrust\EntrustFacade',
-		'Image'   => 'Intervention\Image\Facades\Image',
-		'SEO'     => Artesaos\SEOTools\Facades\SEOTools::class,
-		'SEOMeta'   => Artesaos\SEOTools\Facades\SEOMeta::class,
-		'OpenGraph' => Artesaos\SEOTools\Facades\OpenGraph::class,
-		'Twitter'   => Artesaos\SEOTools\Facades\TwitterCard::class,
+        'LaravelLocalization' => 'Mcamara\LaravelLocalization\Facades\LaravelLocalization',
+        'BrowserDetect' => 'hisorange\BrowserDetect\Facade\Parser',
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        'Entrust' => 'Zizaco\Entrust\EntrustFacade',
+        'Image' => 'Intervention\Image\Facades\Image',
+        'SEO' => Artesaos\SEOTools\Facades\SEOTools::class,
+        'SEOMeta' => Artesaos\SEOTools\Facades\SEOMeta::class,
+        'OpenGraph' => Artesaos\SEOTools\Facades\OpenGraph::class,
+        'Twitter' => Artesaos\SEOTools\Facades\TwitterCard::class,
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
         /*
         laraCMS
         */
         'AdminForm' => App\laraCms\Admin\Facades\AdminForm::class,
-        'Flash'     => 'Helpers\Notifications\Flash',
-        'SEO'       => Artesaos\SEOTools\Facades\SEOTools::class,
-        'Setting'   => \App\laraCms\Tools\SettingHelper::class,
+        'Flash' => 'Helpers\Notifications\Flash',
+        'SEO' => Artesaos\SEOTools\Facades\SEOTools::class,
+        'Setting' => \App\laraCms\Tools\SettingHelper::class,
 
-        'HtmlMenu'       => App\laraCms\Website\Facades\HtmlMenu::class,
-        'HtmlSocial'     => App\laraCms\Website\Facades\HtmlSocial::class,
+        'HtmlMenu' => App\laraCms\Website\Facades\HtmlMenu::class,
+        'HtmlSocial' => App\laraCms\Website\Facades\HtmlSocial::class,
     ],
 
 ];
