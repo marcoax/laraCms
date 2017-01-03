fa28b5042a0413191adc1ba073e1b0350c2bee45
marco asperti <marcoasperti@gmail.com>
2017-01-03 12:07:44 +0100

    javascript files update
    removed unwanted files

--- resources/views/website/app.blade.php
+++ resources/views/website/app.blade.php
@@ -91,7 +91,6 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="//ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 <script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
-<script src="http://www.creolo.org/languages/localize.it.js"></script>
 <script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 <!-- JS Implementing Plugins -->
 <script type="text/javascript" src="{!! asset(config('laraCms.admin.path.assets').'/website/plugins/carousel-swipe.js')!!}"></script>
